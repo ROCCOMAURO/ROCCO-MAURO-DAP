732103fa73d0a257752805eded9c94f72b3a7f7d
Rocco mauro <163151027+ROCCOMAURO@users.noreply.github.com>
2024-04-30 12:55:56 -0300

    Update main.dart

--- TP0_LOGINSIMPLE_MAURO-BLAS/lib/main.dart
+++ TP0_LOGINSIMPLE_MAURO-BLAS/lib/main.dart
@@ -11,10 +11,42 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: SafeArea(
+          child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            Text('Sign In',  
+             style:TextStyle(fontSize: 30, 
+             color:Color.fromRGBO(255, 50, 255, 100),
+             fontWeight: FontWeight.bold,
+
+             ),
+            ),
+
+            SizedBox(height:25),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.person),
+              Text('Blas & Rocco', 
+              style:TextStyle(fontSize: 24),)],
+            ),
+  
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.lock),
+              Text('Password', 
+              style:TextStyle(fontSize: 24),)],
+            )
+            ],
+            ),
         ),
-      ),
-    );
+          
+        ),
+        ),
+      );
   }
 }
