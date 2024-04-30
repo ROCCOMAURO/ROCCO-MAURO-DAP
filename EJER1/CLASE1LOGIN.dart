0b49097cf3c022229741a7f53e0e2e78dfbcfb9f
Rocco mauro <163151027+ROCCOMAURO@users.noreply.github.com>
2024-04-30 11:17:57 -0300

    Create CLASE1LOGIN.dart

---
+++ EJER1/CLASE1LOGIN.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            Text('Sign In',  
+             style:TextStyle(fontSize: 30, 
+             color:Color.fromRGBO(255, 50, 255, 100),
+             fontWeight: FontWeight.bold,
+
+             ),
+            ),
+
+            SizedBox(height:25),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.person),
+              Text('Blas & Rocco', 
+              style:TextStyle(fontSize: 24),)],
+            ),
+  
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.lock),
+              Text('Password', 
+              style:TextStyle(fontSize: 24),)],
+            )
+            ],
+            ),
+        ),
+          
+        ),
+        ),
+      );
+  }
+}
