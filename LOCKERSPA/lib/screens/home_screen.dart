0cc6968cca00aeeff687e3f0df7b03569e4a11d2
Rocco mauro <163151027+ROCCOMAURO@users.noreply.github.com>
2024-06-15 02:13:47 -0300

    hola

--- LOCKERSPA/lib/screens/home_screen.dart
+++ LOCKERSPA/lib/screens/home_screen.dart
@@ -12,75 +12,85 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
-        backgroundColor: Color.fromARGB(255, 216, 99, 213),
-        toolbarHeight: 75,
+        backgroundColor: const Color.fromARGB(255, 216, 99, 213),
+        toolbarHeight: 60,
       ),
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _buildLockerContainer(
+        
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+                      
+              Lockers(
               text: "Locker 1",
-              color: const Color.fromARGB(255, 52, 74, 218),
-              icon: Icons.lock,
-            ),
-            _buildLockerContainer(
-              text: "Locker 2",
-              color: const Color.fromARGB(255, 232, 74, 74),
-              icon: Icons.lock_open,
-              
+                color: const Color.fromARGB(255, 52, 74, 218),
+                icon: Icons.lock,
               ),
-
-            _buildLockerContainer(
-            text: "Locker 3", 
-            color: const Color.fromARGB(201, 58, 225, 255), 
-            icon: Icons.lock, 
-            ),
-
-
-      ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: const Color.fromARGB(225, 60, 243, 43),
-        shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16),
-        ),
-      ),
-      onPressed: () {
-        context.pushNamed(LoginScreen.name);
-      },
-      child: const Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.door_back_door,
-            color: Colors.white,
-            size: 30,
+          
+              Lockers(
+                text: "Locker 2",
+                color: const Color.fromARGB(255, 232, 74, 74),
+                icon: Icons.lock,
+              ),
+          
+              Lockers(
+              text: "Locker 3", 
+              color: const Color.fromARGB(201, 58, 225, 255), 
+              icon: Icons.lock, 
+              ),
+          
+          ElevatedButton(
+          style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(225, 60, 243, 43),  
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
           ),
-          SizedBox(width: 10),
-          Text('Cerrar sesion',
-            style: TextStyle(
-              fontSize: 20,
+                ),
+         onPressed: () {
+          context.pushNamed(LoginScreen.name);
+          const deslog = SnackBar(
+                    duration: Duration(seconds: 3),
+                    content: Text('Cerraste sesion'),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(deslog);
+                },
+                child: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          
+          children: [
+            Icon(
+              Icons.door_back_door_outlined,
               color: Colors.white,
-              fontWeight: FontWeight.bold,
+              size: 30,
             ),
-          ),  
-        ],
-      ),
-    )
-            ,
-          ],
+            SizedBox(width: 10),
+            Text('Cerrar sesion',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ), 
+            
+          ],  
+                ),
+              )
+              ,
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    
   }
 
-  Widget _buildLockerContainer({
+  Widget Lockers({
     required String text,
     required Color color,
     required IconData icon,
+    
   }) {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 10.0),
@@ -90,14 +100,6 @@ class HomeScreen extends StatelessWidget {
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16),
         color: color,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.2),
-            spreadRadius: 2,
-            blurRadius: 4,
-            offset: const Offset(2, 2),
-          ),
-        ],
       ),
       alignment: Alignment.center,
       child: Row(
@@ -121,7 +123,5 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
-
-
   }
 
