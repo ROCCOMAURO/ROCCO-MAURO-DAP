6f64ad7edb404e5d04c993d9a4fd76096364f771
Rocco mauro <163151027+ROCCOMAURO@users.noreply.github.com>
2024-07-24 18:36:26 -0300

    si

--- LOCKERSPA/lib/screens/home_screen.dart
+++ LOCKERSPA/lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tp_listview/screens/locker1_screen.dart';
-//import 'package:tp_listview/screens/locker1_screen.dart';
 import 'package:tp_listview/screens/locker2_screen.dart';
 import 'package:tp_listview/screens/login_screen.dart';
 
