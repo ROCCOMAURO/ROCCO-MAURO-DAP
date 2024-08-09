b0c30737fc681818325b0ad729621eeed93304b9
Rocco mauro <163151027+ROCCOMAURO@users.noreply.github.com>
2024-08-08 22:56:19 -0300

    actualizo

--- LOCKERSPA/README.md
+++
@@ -1,3 +0,0 @@
-# tp_listview
-
-Lista de elementos con sus widgets en home screen y una pequeña descripción.
--- LOCKERSPA/android/app/src/main/kotlin/com/example/tp_listview/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tp_listview
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
